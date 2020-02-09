aa7211ee6368e9cba28412adf9e2dfdf0cd688c3
Themis <themiscadiz@gmail.com>
2020-02-09 12:10:07 -0500

    Assignment 2
    Update3

---
+++ file.js
@@ -0,0 +1,7 @@
+function myClick() {
+    alert("Loisaida /ˌloʊ.iːˈsaɪdə/ is a term derived from the Spanish (and especially Nuyorican) pronunciation of 'Lower East Side'. Originally coined by poet/activist Bittman 'Bimbo' Rivas in his 1974 poem 'Loisaida', it now refers to Avenue C in Alphabet City, whose population has largely been Hispanic (mainly Nuyorican) since the 1960s.");
+}
+
+function loadImage() {
+    alert("Welcome to Loisaida - Lower East Side");
+  }
